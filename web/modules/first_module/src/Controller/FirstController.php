b7f20add96fc077c77c26d308a7e9c7d586395f1
Tuomas Kohvakka <147260100+tauoms@users.noreply.github.com>
2024-08-28 10:23:08 +0300

    Create FirstController.php

---
+++ web/modules/first_module/src/Controller/FirstController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Drupal\first_module\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Symfony\Component\HttpFoundation\Response;
+
+class FirstController extends ControllerBase {
+
+    public function first() {
+        return new Response('Hello My First Module');
+    }
+
+}
