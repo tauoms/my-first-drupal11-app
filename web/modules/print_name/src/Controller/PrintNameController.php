92fd0ca418d643ffd5026c1dfe0305e4d906ce32
Tuomas Kohvakka <147260100+tauoms@users.noreply.github.com>
2024-08-28 12:06:54 +0300

    Refactor PrintNameController to return render array

--- web/modules/print_name/src/Controller/PrintNameController.php
+++ web/modules/print_name/src/Controller/PrintNameController.php
@@ -3,12 +3,15 @@
 namespace Drupal\print_name\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
-use Symfony\Component\HttpFoundation\Response;
+// use Symfony\Component\HttpFoundation\Response;
 
 class PrintNameController extends ControllerBase {
 
     public function print_name() {
-        return new Response('Print name: Tuomas K');
+        //return new Response('Print name: Tuomas K')
+        return [
+            '#markup' => $this->t('Print name: Tuomas K'),
+        ];
     }
 
 }
