23a83e3d7d9eeea01c8de12dd31e22497f4f43f0
Tuomas Kohvakka <147260100+tauoms@users.noreply.github.com>
2024-08-28 10:53:41 +0300

    Create print_name module

---
+++ web/modules/print_name/src/Controller/PrintNameController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Drupal\print_name\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Symfony\Component\HttpFoundation\Response;
+
+class PrintNameController extends ControllerBase {
+
+    public function print_name() {
+        return new Response('Print name: Tuomas K');
+    }
+
+}
