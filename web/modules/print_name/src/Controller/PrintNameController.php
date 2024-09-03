420f4ab642568fba51fd8c0b5d8f99a524a0a9a7
Tuomas Kohvakka <147260100+tauoms@users.noreply.github.com>
2024-09-03 12:05:26 +0300

    Add getting name for greeting from url in PrintNameController

--- web/modules/print_name/src/Controller/PrintNameController.php
+++ web/modules/print_name/src/Controller/PrintNameController.php
@@ -6,15 +6,16 @@ use Drupal\Core\Controller\ControllerBase;
 // use Symfony\Component\HttpFoundation\Response;
 use Drupal\Core\Link;
 use Drupal\Core\Url;
+use Symfony\Component\HttpFoundation\Request;
 
 class PrintNameController extends ControllerBase {
 
-    public function print_name() {
+    public function print_name(Request $request) {
         //return new Response('Print name: Tuomas K')
         $url = Url::fromRoute('print_name.form');
         $link = Link::fromTextAndUrl($this->t('Go to the form'), $url)->toString();
         return [
-            '#markup' => $this->t('Hello, tk! @link', ['@link' => $link])
+            '#markup' => $this->t('Hello, @urlName! @link', ['@link' => $link, '@urlName' => $request->get('url_name')])
         ];
     }
 
