f8ae93687bc5fbfe5b2e5b046164bdc245941299
Tuomas Kohvakka <147260100+tauoms@users.noreply.github.com>
2024-09-03 10:33:10 +0300

    Link to form from path /printname

--- web/modules/print_name/src/Controller/PrintNameController.php
+++ web/modules/print_name/src/Controller/PrintNameController.php
@@ -4,13 +4,17 @@ namespace Drupal\print_name\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
 // use Symfony\Component\HttpFoundation\Response;
+use Drupal\Core\Link;
+use Drupal\Core\Url;
 
 class PrintNameController extends ControllerBase {
 
     public function print_name() {
         //return new Response('Print name: Tuomas K')
+        $url = Url::fromRoute('print_name.form');
+        $link = Link::fromTextAndUrl($this->t('Go to the form'), $url)->toString();
         return [
-            '#markup' => $this->t('Print name: Tuomas K'),
+            '#markup' => $this->t('Hello, tk! @link', ['@link' => $link])
         ];
     }
 
