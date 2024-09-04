26d258443d65ee833d0c30f2ce6788db39cdb035
Tuomas Kohvakka <147260100+tauoms@users.noreply.github.com>
2024-09-04 12:26:12 +0300

    Create links_example module files

--- web/modules/links_example/src/Controller/LinksExampleController.php
+++ web/modules/links_example/src/Controller/LinksExampleController.php
@@ -7,9 +7,19 @@ use Drupal\Core\Controller\ControllerBase;
 use Drupal\Core\Link;
 use Drupal\Core\Url;
 
+// paths for links:
+// block.admin_display = /admin/structure/block
+// system.admin_content	 = /admin/content
+// entity.user.collection = /admin/people	
+// <front> = /
+
 class LinksExampleController extends ControllerBase {
 
-    public function links_example() {
-
+    public function displayLinks() {
+        $url = Url::fromRoute('block.admin_display');
+        $link = Link::fromTextAndUrl($this->t('Block Admin Display'), $url)->toString();
+        return [
+            '#markup' => $this->t('Admin links: @link', ['@link' => $link])
+        ];
     }
 }
