9e9e5a0b0ca9e541e67e4a0f4877a763f68b0380
Tuomas Kohvakka <147260100+tauoms@users.noreply.github.com>
2024-09-04 10:54:00 +0300

    Create base for links_example module

---
+++ web/modules/links_example/src/Controller/LinksExampleController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Drupal\links_example\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+// use Symfony\Component\HttpFoundation\Response;
+use Drupal\Core\Link;
+use Drupal\Core\Url;
+
+class LinksExampleController extends ControllerBase {
+
+    public function links_example() {
+
+    }
+}
