415a0bf3119fa99ece9c798e0e51d7f959d078bc
Tuomas Kohvakka <147260100+tauoms@users.noreply.github.com>
2024-09-17 13:30:07 +0300

    react_list module working

--- web/modules/custom/react_list/js/react_list/src/main.jsx
+++ web/modules/custom/react_list/js/react_list/src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { StrictMode } from "react";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById("react-app")).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
-)
+  </React.StrictMode>
+);
