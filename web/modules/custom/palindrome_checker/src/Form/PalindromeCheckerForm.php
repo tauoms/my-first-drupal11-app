a85b0906332655fa715d6f473aa44c38182f5ef8
Tuomas Kohvakka <147260100+tauoms@users.noreply.github.com>
2024-09-25 22:19:39 +0300

    Add logic for palindrome checking

--- web/modules/custom/palindrome_checker/src/Form/PalindromeCheckerForm.php
+++ web/modules/custom/palindrome_checker/src/Form/PalindromeCheckerForm.php
@@ -27,7 +27,23 @@ class PalindromeCheckerForm extends FormBase {
 
     public function submitForm(array &$form, FormStateInterface $form_state)
     {
-        $word = $form_state->getValue('word');
-        \Drupal::messenger()->addMessage($this->t('Yes! @word is a palindrome!', ['@word' => $word]));
+        $inputStr = $form_state->getValue('word');
+
+        if (!empty($inputStr) && !preg_match('~[0-9]+~', $inputStr) && !preg_match('/[\'^£€$%&*()}{@#~?><>,|=_+¬-]/', $inputStr)) { 
+			if (strtolower($inputStr) === strrev(strtolower($inputStr))) {
+                \Drupal::messenger()->addMessage($this->t('Yes! @word IS a palindrome!', ['@word' => $inputStr]));			
+            } else {
+                \Drupal::messenger()->addMessage($this->t('Sadly @word is NOT a palindrome.', ['@word' => $inputStr]));			
+            }
+
+		} else if (!empty($inputStr) && preg_match('~[0-9]+~', $inputStr)) {
+			\Drupal::messenger()->addMessage($this->t('You entered numbers. Please enter a word with only letters.', ['@word' => $inputStr]));
+		
+		} else if (!empty($inputStr) && preg_match('/[\'^£€$%&*()}{@#~?><>,|=_+¬-]/', $inputStr)) {
+			\Drupal::messenger()->addMessage($this->t("Please don't include special characters in your input.", ['@word' => $inputStr]));
+			
+		}
+
+        
     }
 }
